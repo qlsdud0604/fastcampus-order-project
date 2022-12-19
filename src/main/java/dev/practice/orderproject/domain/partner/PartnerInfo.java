07c020c437d9c83e6d2638203f1bcabe20cfa8d8
임빈영 <qlsdud960604@gmail.com>
2022-12-19 15:04:08 +0900

    Refactoring #3 - PartnerInfo 클래스
    Partner 클래스를 매개변수로 받는 생성자 추가

--- src/main/java/dev/practice/orderproject/domain/partner/PartnerInfo.java
+++ src/main/java/dev/practice/orderproject/domain/partner/PartnerInfo.java
@@ -13,4 +13,13 @@ public class PartnerInfo {
     private String businessNo;
     private String email;
     private Partner.Status status;
+
+    public PartnerInfo(Partner partner) {
+        this.id = partner.getId();
+        this.partnerToken = partner.getPartnerToken();
+        this.partnerName = partner.getPartnerName();
+        this.businessNo = partner.getBusinessNo();
+        this.email = partner.getEmail();
+        this.status = partner.getStatus();
+    }
 }
