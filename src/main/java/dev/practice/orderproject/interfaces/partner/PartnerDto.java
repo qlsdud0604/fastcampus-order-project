8b3cad66b050fc57575dbfa0c4bbdcedb35425bc
임빈영 <qlsdud960604@gmail.com>
2022-12-20 14:06:46 +0900

    Refactoring #13 - PartnerDto 클래스
    RegisterRequest 객체를 PartnerCommand 클래스로 변환하기 위한 toCommand() 메서드와 PartnerInfo 객체를 매개변수로 받아 RegisterRepsonse 객체를 생성하기 위한 생성자 추가

--- src/main/java/dev/practice/orderproject/interfaces/partner/PartnerDto.java
+++ src/main/java/dev/practice/orderproject/interfaces/partner/PartnerDto.java
@@ -1,6 +1,8 @@
 package dev.practice.orderproject.interfaces.partner;
 
 import dev.practice.orderproject.domain.partner.Partner;
+import dev.practice.orderproject.domain.partner.PartnerCommand;
+import dev.practice.orderproject.domain.partner.PartnerInfo;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,10 +17,17 @@ public class PartnerDto {
         private String partnerName;
         private String businessNo;
         private String email;
+
+        public PartnerCommand toCommand() {
+            return PartnerCommand.builder()
+                    .partnerName(partnerName)
+                    .businessNo(businessNo)
+                    .email(email)
+                    .build();
+        }
     }
 
     @Getter
-    @Builder
     @ToString
     public static class RegisterResponse {
         private final String partnerToken;
@@ -26,5 +35,13 @@ public class PartnerDto {
         private final String businessNo;
         private final String email;
         private final Partner.Status status;
+
+        public RegisterResponse(PartnerInfo partnerInfo) {
+            this.partnerToken = partnerInfo.getPartnerToken();
+            this.partnerName = partnerInfo.getPartnerName();
+            this.businessNo = partnerInfo.getBusinessNo();
+            this.email = partnerInfo.getEmail();
+            this.status = partnerInfo.getStatus();
+        }
     }
 }
