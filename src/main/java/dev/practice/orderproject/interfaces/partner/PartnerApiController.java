b7e42c5f5965b3495ff9d9222e42e533960cbf87
임빈영 <qlsdud960604@gmail.com>
2022-12-20 14:58:54 +0900

    Refactoring #18 - PartnerApiController 클래스
    @RequestBody 애너테이션을 활용하여 PartnerDto 객체와 body 데이터가 매핑이 되도록 개선, @Valid 애너테이션을 활용하여 PartnerDto 클래스 내 필드들의 유효성을 검사

--- src/main/java/dev/practice/orderproject/interfaces/partner/PartnerApiController.java
+++ src/main/java/dev/practice/orderproject/interfaces/partner/PartnerApiController.java
@@ -5,8 +5,11 @@ import dev.practice.orderproject.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
@@ -17,7 +20,8 @@ public class PartnerApiController {
     private final PartnerFacade partnerFacade;
 
     @PostMapping
-    public CommonResponse registerPartner(PartnerDto.RegisterRequest request) {
+    public CommonResponse registerPartner(@RequestBody @Valid PartnerDto.RegisterRequest request) {
+        log.info(request.toString());
         var command = request.toCommand();
         var partnerInfo = partnerFacade.registerPartner(command);
         var response = new PartnerDto.RegisterResponse(partnerInfo);
